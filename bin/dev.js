a4ac6dc042785c3d3ba0082af93688292e9592f4
Cesar Kohl <cesarkohl@gmail.com>
2020-08-03 18:51:39 -0300

    Update for /bin on npm i.

---
+++ bin/dev.js
@@ -0,0 +1,26 @@
+const { exec } = require('child_process');
+
+function init() {
+
+  let cmd = `
+    live-server --port=3000 --open=src &
+    node-sass -w src/ -o src/ &
+    tsc-watch
+  `;
+
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.warn(
+        "ERROR: \n" +
+        cmd + "\n" +
+        "Return: " + error
+      );
+    } else {
+      console.log('.');
+    }
+    console.log('Done!');
+  });
+
+}
+
+init();
