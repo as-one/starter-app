efd168a70d1bea101c27a97fa82796e3c4fef668
Cesar Kohl <cesarkohl@gmail.com>
2020-08-05 10:47:33 -0300

    Add concurrently. Update nodeSass output folder.

--- src/dev/main.js
+++ src/dev/main.js
@@ -4,7 +4,7 @@ const { exec } = require('child_process');
 
 export async function starterAppDev(options) {
   let liveServer = `live-server --port=3000 --open=${options.input}`;
-  let nodeSass = `node-sass -w ${options.input} -o ${options.output}`;
+  let nodeSass = `node-sass -w ${options.input} -o ${options.input}`;
   let tscWatch = "tsc-watch";
 
   let cmd = `concurrently "${liveServer}" "${nodeSass}" "${tscWatch}"`;
